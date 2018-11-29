19705ef290b93fe6c01590f977b8f3f5b4001585
Wani4ka <contact.wani4ka@gmail.com>
2018-11-29 20:58:19 +0300

    Initial commit

--- src/me/wani4ka/lobby/Lobby.java
+++ src/me/wani4ka/lobby/Lobby.java
@@ -1,6 +1,9 @@
 package me.wani4ka.lobby;
 
-import org.bukkit.*;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.WeatherType;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
@@ -14,7 +17,6 @@ import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerGameModeChangeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -25,7 +27,6 @@ public class Lobby extends JavaPlugin implements Listener {
 
 	private final List<String> allowedCmds = new ArrayList<>();
 	private String cmdDeniedMsg;
-	private Location spawn;
 
 	@Override
 	public void onEnable() {
@@ -37,44 +38,14 @@ public class Lobby extends JavaPlugin implements Listener {
 		FileConfiguration config = YamlConfiguration.loadConfiguration(f);
 		allowedCmds.addAll(config.getStringList("allowed-cmds"));
 		cmdDeniedMsg = ChatColor.translateAlternateColorCodes('&', config.getString("cmd-denied-msg", "&eКоманда не найдена!"));
-		parseSpawn(config.getString("spawn", "0:100:0:0:0"));
-	}
-
-	private void parseSpawn(String source) {
-		String[] loc = source.split(":");
-		int x, y, z;
-		float yaw, pitch;
-		try {
-			x = Integer.parseInt(loc[0]);
-			y = Integer.parseInt(loc[1]);
-			z = Integer.parseInt(loc[2]);
-			yaw = Float.parseFloat(loc[3].replace(',', '.'));
-			pitch = Float.parseFloat(loc[4].replace(',', '.'));
-		} catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
-			x = 0;
-			y = 100;
-			z = 0;
-			yaw = 0;
-			pitch = 0;
-			e.printStackTrace();
-		}
-		spawn = new Location(Bukkit.getWorld("world"), (double) x+0.5, y, (double) z+0.5, yaw, pitch);
 	}
 
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e) {
 		e.getPlayer().setGameMode(GameMode.ADVENTURE);
-		e.getPlayer().teleport(spawn);
 		e.getPlayer().setPlayerWeather(WeatherType.CLEAR);
 		e.getPlayer().setPlayerTime(16000, false);
 		e.getPlayer().setFlySpeed(0.2f);
-	}
-
-	@EventHandler(ignoreCancelled = true)
-	public void onFall(PlayerMoveEvent e) {
-		if (e.getTo().getY() < 1.0) {
-			e.getPlayer().teleport(spawn);
-		}
 	}
 
 	@EventHandler(ignoreCancelled = true)
@@ -87,12 +58,8 @@ public class Lobby extends JavaPlugin implements Listener {
 
 	@EventHandler
 	public void onDamage(EntityDamageEvent e) {
-		if (e.getEntity() instanceof Player) {
+		if (e.getEntity() instanceof Player)
 			e.setCancelled(true);
-			if (e.getFinalDamage() >= ((Player) e.getEntity()).getHealth()) {
-				e.getEntity().teleport(spawn);
-			}
-		}
 	}
 
 	@EventHandler
